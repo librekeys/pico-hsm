ca34955b9185175f41536891e941b6692fcc8c2e
Fabrice Bellamy <38373466+Lab-8916100448256@users.noreply.github.com>
2026-02-27 15:13:35 +0100

    Merge pull request #1 from librekeys/licensing-cleanup
    update licensing info for this fork of the community edition

--- tools/pico-hsm-tool.py
+++ tools/pico-hsm-tool.py
@@ -549,7 +549,7 @@ def parse_derivation_path(path):
 def main(args):
     sys.stderr.buffer.write(b'Pico HSM Tool v2.4\n')
     sys.stderr.buffer.write(b'Author: Pol Henarejos\n')
-    sys.stderr.buffer.write(b'Report bugs to https://github.com/polhenarejos/pico-hsm/issues\n')
+    sys.stderr.buffer.write(b'Report bugs to https://github.com/librekeys/pico-hsm/issues\n')
     sys.stderr.buffer.write(b'\n\n')
     sys.stderr.flush()
 
